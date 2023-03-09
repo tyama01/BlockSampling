39d3c6ac9d7d157103890a99894a00f3cba77c54
tyama01 <tyama@inl.ics.keio.ac.jp>
2023-03-09 14:09:54 +0900

    commit

---
+++ include/graph.h
@@ -0,0 +1,33 @@
+#ifndef GUARD_GRAPH_H
+#define GUARD_GRAPH_H
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Graph{
+    private:
+        unordered_set<int> node_list_set;
+        unordered_map<int, vector<int>> adjacency_list; // ノードid -> 隣接ノードのvector
+
+    public:
+        // getter 関数
+        const vector<int> get_node_list_id_sort();
+        const unordered_map<int, vector<int>>& get_adjacency_list();
+        const int get_number_of_nodes();
+        const int get_number_of_edges();
+
+        // グラフの操作
+        void add_edge(int n1, int n2);
+
+        // 頂点IDをブロック化
+        vector<vector<int>> get_block_id(double per_block_ratio);
+
+        // グラフ演算
+        unordered_map<int, double> pagerank();
+        
+};
+
+#endif // GURAD_GRAPH_H
---
+++ include/read.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_READ_H
+#define GUARD_READ_H
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+class Graph;
+
+void read_graph_from_text_file(string file_path, Graph& graph);
+
+#endif // GUAD_READ_H
