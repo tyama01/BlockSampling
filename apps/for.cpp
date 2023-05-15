6e1573a32b8a8665196946ec492b385cca036eea
tyama01 <tyama@inl.ics.keio.ac.jp>
2023-05-15 11:25:38 +0900

    commit

--- apps/for.cpp
+++ apps/for.cpp
@@ -7,8 +7,8 @@ int main(){
 
     clock_t start = clock(); // 計測開始
 
-    for(int i = 0; i < 1000; i++){
-        for(int j = 0; j < 1000; j++){
+    for(int i = 0; i < 100000; i++){
+        for(int j = 0; j < 100000; j++){
             x*y;
         }
     }
