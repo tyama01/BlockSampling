e1818b8aed754c627b4e0824f434ccdeef4ebc06
tyama01 <tyama@inl.ics.keio.ac.jp>
2023-05-15 10:29:05 +0900

    commit

---
+++ apps/for.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <time.h>
+
+
+int main(){
+    int x, y;
+
+    clock_t start = clock(); // 計測開始
+
+    for(int i = 0; i < 1000; i++){
+        for(int j = 0; j < 1000; j++){
+            x*y;
+        }
+    }
+
+    clock_t end = clock(); // 計測終了
+
+    double time = (double)(end - start) / CLOCKS_PER_SEC*1000;
+    printf("%.2f ms\n", time);
+
+
+    return 0;
+}
